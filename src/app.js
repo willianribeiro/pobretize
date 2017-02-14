2083acab2c151af41153e3e24e24938148aacbaf
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-02-14 19:37:03 -0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React from 'react'
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                Pobretize! (-;
+            </div>
+        )
+    }
+}
+    
+export default App
